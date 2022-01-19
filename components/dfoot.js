b14650b659f7f0ee57effdb1189192c591ff5849
Dhiraj-Giri <dgiri5065@gmail.com>
2022-01-19 17:20:01 +0530

    header / footer

---
+++ components/dfoot.js
@@ -0,0 +1,120 @@
+
+function footer(){
+    return `<div id="dbox1">
+    <div class="dlft">
+        <div>
+            <p> OUR COLLECTIONS</p>
+            <p>Boosters</p>
+            <p>Calm</p>
+            <p>Clear</p>
+            <p>Clinical</p>
+            <p>Defense</p>
+            <p>Earth Sourced</p>
+            <p>Exfoliant</p>
+            <p>Hydralight</p>
+        </div>
+        <div>
+            <p>Moisture Boost</p>
+            <p>Omega+ Complex</p>
+            <p>PC4Men</p>
+            <p>Resist</p>
+            <p>Skin Balancing</p>
+            <p>Skin Perfecting</p>
+            <p>Skin Recovery</p>
+            <p>View All</p>
+        </div>
+    </div>
+    <div class="drgt">
+        <div id="dtp">
+            <div>
+                <p>WHO WE ARE</p>
+                <p><a href="/about.html"> About Us </a></p>
+                <p>Paula's Story</p>
+                <p>Paula's Choice Perks</p>
+                <p>COVID-19 Information</p>
+                <p>Sustainability & Recycling</p>
+                <p>Careers</p>
+                <p>Auto Replenishment</p>
+                <p>Science Advisory Board</p>
+                <p>Research & Education Team</p>
+                <p>On The Rise Program</p>
+            </div>
+            <div>
+                <p>CLIENT SERVICES</p>
+                <p>Order Status</p>
+                <p>Help Center</p>
+                <p>Private Consultation</p>
+                <p>My Account</p>
+                <p>Email Us</p>
+            </div>
+            <div>
+                <p>PARTNERS</p>
+                <p>Student Discount</p>
+                <p>Affiliate Program</p>
+                <p>International</p>
+            </div>
+        </div>
+        <div id="dmd">
+            <div>
+                <p>SIGN UP FOR SPECIAL OFFERS</p>
+                <div>
+                    <input type="text" placeholder="Enter your email">
+                    <button id="dsub">SIGN UP</button>
+                </div>
+            </div>
+            <div>
+                <p>1.800.831.4088</p>
+                <div>
+                   <a href="https://www.facebook.com/PaulasChoice.Inc"> <i class="fab fa-facebook-f"></i></a>
+                   <a href="https://twitter.com/paulaschoice"><i class="fab fa-twitter"></i></a>
+                   <a href="https://www.youtube.com/user/PaulasChoiceBeauty"> <i class="fab fa-youtube"></i></a>
+                   <a href="https://www.pinterest.com/paulaschoice"> <i class="fab fa-pinterest-p"></i></a>
+                   <a href="https://www.instagram.com/paulaschoice"> <i class="fab fa-instagram"></i></a>
+                </div>
+            </div>
+        </div>
+        <div id="dtd">
+            <div>
+                <p>PAYMENT OPTIONS</p>
+                <div>
+                    <img src="https://www.paulaschoice.com/on/demandware.static/-/Library-Sites-paulachoice/default/dw8a429350/images/footer/desktop-payment-icons.png" alt="">
+                </div>
+            </div>
+            <div>
+                <p>SAFE & SECURE</p>
+                <div>
+                    <img src="https://www.paulaschoice.com/on/demandware.static/-/Library-Sites-paulachoice/default/dw0f07a73e/images/footer/mcafee-60.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="dbox2">
+    <div class="dabt">
+        <p>© Copyright 2021 Paula's Choice.
+             All rights reserved.
+             Paula's Choice and Cosmetics Cop are
+              registered trademarks of Paula's Choice, LLC.</p>
+    </div>
+    <div class="dtrms">
+        <div>
+            <p>TERMS OF USE</p>
+        </div>
+        <div>
+            <p>ACCESSIBILITY</p>
+        </div>
+        <div>
+            <p>PRIVACY/SECURITY</p>
+        </div>
+        <div>
+            <p>DON'T SELL MY DATA</p>
+        </div>
+        <div>
+            <p>SITE MAP</p>
+        </div>
+    </div>
+</div>`
+
+}
+
+export default footer
---
+++ components/dnav.js
@@ -0,0 +1,63 @@
+function navbar(){
+
+    return ` <div id="dfrstbr">
+    <div>
+        <p>Free Shipping on $49</p>
+    </div>
+    <div>
+        <p>20% Off 2+ Items</p>
+    </div>
+    <div>
+        <p>Paula's Choice Perks <a href="">Get Started</a></p>
+    </div>
+</div>
+<div id="dscndbr">
+    <div class="dsignoffer">
+        <p><i class="far fa-envelope"></i>Sign-Up for Special Offers</p>
+    </div>
+    <div class="dpaula">
+        <a href="">
+        <h1>PAULA'S CHOICE</h1>
+        <h3>SKINCARE</h3>
+    </a>
+    </div>
+    <div class="dsearch">
+        <input type="text" id="dsrch" placeholder="Search for skin care">
+        <button id="dsym">
+            <i class="fas fa-search"></i>
+        </button>
+    </div>
+    <div class="daccount">
+        <h3 id="user">SIGN IN </h3>
+        <a href=""><i class="fas fa-user"></i></a>
+    </div>
+    <div class="dbag">
+        <h3>BAG</h3>
+        <a href=""><i class="fas fa-shopping-bag"></i></a>
+        <p id="dcount">0</p>
+    </div>
+</div>
+<div id="dthrdbr">
+    <div class="dscp">
+        <p>SKIN CARE PRODUCTS</p>
+    </div>
+    <div class="dsc">
+        <p>SKIN CONCERNS</p>
+    </div>
+    <div class="dst">
+        <p>SKIN TYPE</p>
+    </div>
+    <div class="di">
+        <p>INGREDIENTS</p>
+    </div>
+    <div class="dt">
+        <p>TRENDING</p>
+    </div>
+    <div class="dscr">
+        <p>SKIN CARE RESOURCES</p>
+    </div>
+</div>`
+}
+
+
+export default navbar
